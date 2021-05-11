390f3410d673eae44061d0f08011d19381294a47
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-11 18:46:53 -0400

    directory structure

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+
+
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+});
