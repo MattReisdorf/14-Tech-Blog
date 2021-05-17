575d01d3f47011d37b940e67cd1eba9124c31307
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-16 22:19:28 -0400

    dashboard maybe working i really hope so im so done with the homework but i cant test anything until jeremy pushes the signup stuff but who knows if that will work so idk whats gonna happen

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,6 +24,33 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get('/dashboard', async(req, res) => {
+
+  const postData = await Post.findAll({
+    where: {
+      user_id: req.user.session
+    },
+    include: {
+      model: User, 
+      attributes: [
+        'id',
+        'name',
+        'email'
+      ]
+    }
+  })
+
+  const posts = postData.map((post) => post.get({ plain: true }));
+
+
+  res.render('posts', {
+    logged_in: req.session.logged_in,
+    posts
+  })
+
+
+})
+
 router.get("/api/user/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");
