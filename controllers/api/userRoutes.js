d1d17fe1b47e51a00e7fbc2bbdf9771f9067582e
JeremyJ <JeremyJoanet@protonmail.com>
2021-05-16 21:32:05 -0500

    added signup feature

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -14,10 +14,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-
-
-
-
 router.post('/login', async (req, res) => {
   console.log("logggggged in")
   try {
@@ -58,6 +54,25 @@ router.post('/logout', (req, res) => {
     });
   } else {
     res.status(404).end();
+  }
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+})
+
+router.post('/signup', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData)
+    })
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
